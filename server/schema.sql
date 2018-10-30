99246976b9b87dd477f1a90effb509be9e7f2327
Casey Shamey <caseyshamey@gmail.com>
2018-10-29 22:54:33 -0400

    Complete schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,17 +2,95 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-<<<<<<< HEAD
-  id VARCHAR(20) primary key,
-  username VARCHAR(20),
-  message VARCHAR(20),
-  createdAt DATE
-=======
+/* CREATE TABLE messages (
   /* Describe your table here.*/
 
->>>>>>> 14752851c7a44492f790162680d715cb0b242db1
+--); */
+
+
+-- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'messages'
+--
+-- ---
+
+DROP TABLE IF EXISTS `messages`;
+
+CREATE TABLE `messages` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `message` VARCHAR(250) NULL DEFAULT NULL,
+  `created at` DATETIME NULL DEFAULT NULL,
+  `id_users` INTEGER NULL DEFAULT NULL,
+  `id_rooms` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
+
+-- ---
+-- Table 'rooms'
+--
+-- ---
+
+DROP TABLE IF EXISTS `rooms`;
+
+CREATE TABLE `rooms` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `room` VARCHAR(50) NULL DEFAULT NULL,
+  `id_messages` INTEGER NULL DEFAULT NULL,
+  `id_users` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'users'
+--
+-- ---
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `user` VARCHAR(50) NULL DEFAULT NULL,
+  `id_messages` INTEGER NULL DEFAULT NULL,
+  `id_rooms` INTEGER NULL DEFAULT NULL,
+  `friends` VARCHAR(50) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Foreign Keys
+-- ---
+
+ALTER TABLE `messages` ADD FOREIGN KEY (id_users) REFERENCES `users` (`id`);
+ALTER TABLE `messages` ADD FOREIGN KEY (id_rooms) REFERENCES `rooms` (`id`);
+ALTER TABLE `rooms` ADD FOREIGN KEY (id_messages) REFERENCES `messages` (`id`);
+ALTER TABLE `rooms` ADD FOREIGN KEY (id_users) REFERENCES `users` (`id`);
+ALTER TABLE `users` ADD FOREIGN KEY (id_messages) REFERENCES `messages` (`id`);
+ALTER TABLE `users` ADD FOREIGN KEY (id_rooms) REFERENCES `rooms` (`id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `rooms` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `messages` (`id`,`message`,`created at`,`id_users`,`id_rooms`) VALUES
+-- ('','','','','');
+-- INSERT INTO `rooms` (`id`,`room`,`id_messages`,`id_users`) VALUES
+-- ('','','','');
+-- INSERT INTO `users` (`id`,`user`,`id_messages`,`id_rooms`,`friends`) VALUES
+-- ('','','','','');
 
 /* Create other tables and define schemas for them here! */
 
